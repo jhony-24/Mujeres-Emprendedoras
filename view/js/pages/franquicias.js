43e1783445d6e822719ba13e3a442136084d21d8
jhony <jhonyvega24@gmail.com>
2019-12-21 07:41:27 -0500

    card responsive de la pagina franquicias

--- view/js/pages/franquicias.js
+++ view/js/pages/franquicias.js
@@ -1,19 +1,26 @@
 var franquicias = function() {
 
   var data = {
+
     items : () => document.getElementsByClassName("container-list"),
     header : () => document.getElementsByClassName("header"),
     body : () => document.getElementsByClassName("content-body"),
-    hide : function(e) {
+    icon : () => document.getElementsByClassName("icon"),
+
+    hide : function(e,index) {
       e.style.display = "none";
+      this.header()[index].style.color = "rgb(80,80,80)";
+      //this.icon()[index].className = "fa fa-plus";
     },
-    show : function(e) {
+    show : function(e,index) {
       e.style.display = "block";
+      this.header()[index].style.color = "rgb(230,60,90)";
+      //this.icon()[index].className = "fa fa-minus";
     },
 
     hideAll : function() {
       Array.from(this.items()).forEach( ( item , index) => {
-        this.hide(this.body()[index]);
+        this.hide(this.body()[index],index);
       });
     },
 
@@ -21,9 +28,9 @@ var franquicias = function() {
       Array.from(this.header()).forEach( ( header , index ) => {
           var state = true;
           var body = this.body()[index - 1];
-          header.onclick = () =>  {
-            if(state) this.show(body);
-            else  this.hide(body);
+          header.onclick = () => {
+            if(state) this.show(body,index);
+            else  this.hide(body,index);
             state = !state;
           }
       });
